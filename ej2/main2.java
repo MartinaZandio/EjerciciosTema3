1e183fbc0b0a70f99f2b66188369e3312e6adf9a
MartinaZandio <159145221+MartinaZandio@users.noreply.github.com>
2024-03-10 11:57:01 +0000

    ej2

---
+++ ej2/main2.java
@@ -0,0 +1,11 @@
+/*Crea una clase fecha que almacene el día, el mes y el año de una fecha. Proporciona funciones miembro para acceder a 
+estos atributos (getDia(), getMes() y getAño() y para modificarlos (setDia(int dia), setMes(int mes) y setAño(int año)). 
+Sobreescribe su método toString(). Crea la fecha 20/10/2018. Muéstrala por pantalla. Después cambia el año 2019. 
+Muéstrala por pantalla. */
+
+public class main2(){
+    fecha fecha1 = new fecha (20,10,2018);
+    System.out.println(fecha1);
+    fecha1.setAño(2019);
+    sout(fecha1);
+}
